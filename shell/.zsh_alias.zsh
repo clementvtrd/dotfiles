357d0d949330006cca34e179754991ccf5f60402
Clément Vétillard <84911237+clementvtrd@users.noreply.github.com>
2022-10-03 13:15:27 +0200

    feature: add docker compose aliases

--- shell/.zsh_alias.zsh
+++ shell/.zsh_alias.zsh
@@ -1,1 +1,6 @@
 alias ls="exa --icons --group-directories-first --long --no-permissions --octal-permissions --no-time"
+
+# Docker compose
+alias dc="docker compose"
+alias dce="docker compose exec"
+alias dcr="docker compose run --rm"
