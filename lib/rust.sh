1d12aa956eaac81c3366f76416ef2b72e1126071
clementvtrd <84911237+clementvtrd@users.noreply.github.com>
2022-10-12 16:30:46 +0200

    fix: not compatible with wayland

--- lib/rust.sh
+++ lib/rust.sh
@@ -5,10 +5,14 @@ rustup override set stable
 rustup update stable
 
 git clone https://github.com/alacritty/alacritty.git
+
 cd alacritty
 
-cargo build --release --no-default-features --features=wayland
+cargo build --release
 sudo cp target/release/alacritty /usr/local/bin
 sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
 sudo desktop-file-install extra/linux/Alacritty.desktop
 sudo update-desktop-database
+
+cd ../
+rm -rf alacritty
