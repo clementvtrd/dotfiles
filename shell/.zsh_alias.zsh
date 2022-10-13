e92a0aae3180fdd00efada81aac06cd21ff2f702
clementvtrd <84911237+clementvtrd@users.noreply.github.com>
2022-10-13 20:44:47 +0200

    feature: add alias to kubectl via microk8s

--- shell/.zsh_alias.zsh
+++ shell/.zsh_alias.zsh
@@ -4,3 +4,6 @@ alias ls="exa --icons --group-directories-first --long --no-permissions --octal-
 alias dc="docker compose"
 alias dce="docker compose exec"
 alias dcr="docker compose run --rm"
+
+# Kubernetes
+alias kubectl="microk8s kubectl"
