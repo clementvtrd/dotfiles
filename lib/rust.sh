04f09849257893fb3d7c2ab41681bb7919ae3ce4
clementvtrd <84911237+clementvtrd@users.noreply.github.com>
2022-10-13 20:44:04 +0200

    fix: rust

--- lib/rust.sh
+++ lib/rust.sh
@@ -1,5 +1,6 @@
 sudo apt install curl cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME/.cargo/env"
 
 rustup override set stable
 rustup update stable
